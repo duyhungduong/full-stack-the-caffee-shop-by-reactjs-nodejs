0f679dcb057e7a77a4e77e3b92d79182c65774bf
duyhungduong <duyhungduong10@gmail.com>
2024-11-22 11:09:54 +0700

    Update booking

--- backend/helper/emailHelper.js
+++ backend/helper/emailHelper.js
@@ -77,13 +77,10 @@ async function sendBookingConfirmationEmail(
     ],
   };
 
-  try {
+ 
     await transporter.sendMail(mailOptions);
     console.log("Email sent successfully!");
-  } catch (error) {
-    console.error("Error sending email:", error);
-    throw new Error("Failed to send email");
-  }
+  
 }
 
 module.exports = { sendBookingConfirmationEmail };
